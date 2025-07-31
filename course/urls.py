2c6931eead613e6bd9239348c5de9826443cf6b3
Nikolay Korkushko <kakashonochka12@gmail.com>
2025-07-31 22:45:30 +0300

    homework_30_1 part 3 fix

--- course/urls.py
+++ course/urls.py
@@ -12,9 +12,9 @@ router.register(r'course', CourseViewSet, basename='course')
 urlpatterns = router.urls
 
 urlpatterns = [
-    path('lesson/', LessonAPIView.as_view(), name='lesson_list'),
-    path('lesson/create/', LessonAPICreate.as_view(), name='lesson_create'),
-    path('lesson/update/<int:pk>', LessonAPIUpdate.as_view(), name='lesson_update'),
-    path('lesson/destroy/<int:pk>', LessonAPIDestroy.as_view(), name='lesson_destroy'),
-    path('lesson/update/<int:pk>', LessonList.as_view(), name='lesson_update'),
+    path('', LessonAPIView.as_view(), name='lesson_list'),
+    path('create/', LessonAPICreate.as_view(), name='lesson_create'),
+    path('update/<int:pk>', LessonAPIUpdate.as_view(), name='lesson_update'),
+    path('destroy/<int:pk>', LessonAPIDestroy.as_view(), name='lesson_destroy'),
+    path('update/<int:pk>', LessonList.as_view(), name='lesson_update'),
 ] + router.urls
