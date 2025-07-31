1655b4b783bfef15c28c18be6947ae8e6bf64dd2
Nikolay Korkushko <kakashonochka12@gmail.com>
2025-07-31 23:05:36 +0300

    homework_30_1 part 3 fix

--- course/urls.py
+++ course/urls.py
@@ -11,10 +11,4 @@ router = DefaultRouter()
 router.register(r'course', CourseViewSet, basename='course')
 urlpatterns = router.urls
 
-urlpatterns = [
-    path('', LessonAPIView.as_view(), name='lesson_list'),
-    path('create/', LessonAPICreate.as_view(), name='lesson_create'),
-    path('update/<int:pk>', LessonAPIUpdate.as_view(), name='lesson_update'),
-    path('destroy/<int:pk>', LessonAPIDestroy.as_view(), name='lesson_destroy'),
-    path('detail/<int:pk>', LessonList.as_view(), name='lesson_detail'),
-] + router.urls
+urlpatterns = [] + router.urls
--- lesson/urls.py
+++ lesson/urls.py
@@ -1,14 +1,21 @@
+from django.urls import path
 from rest_framework.routers import DefaultRouter
 
 from course.apps import CourseConfig
 from course.views import CourseViewSet
+from lesson.views import LessonAPIView, LessonAPICreate, LessonAPIUpdate, LessonAPIDestroy, LessonList
+
 
 app_name = CourseConfig.name
 
 router = DefaultRouter()
-router.register(r'lesson', CourseViewSet, basename='course')
+router.register(r'lesson', CourseViewSet, basename='lesson')
 urlpatterns = router.urls
 
 urlpatterns = [
-
+      path('', LessonAPIView.as_view(), name='list'),
+      path('create/', LessonAPICreate.as_view(), name='create'),
+      path('update/<int:pk>', LessonAPIUpdate.as_view(), name='update'),
+      path('destroy/<int:pk>', LessonAPIDestroy.as_view(), name='destroy'),
+      path('detail/<int:pk>', LessonList.as_view(), name='detail'),
 ] + router.urls
