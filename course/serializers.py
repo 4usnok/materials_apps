58836943dcd209ecb7a1f27fe9fe0ac9bce1ebb2
Nikolay Korkushko <kakashonochka12@gmail.com>
2025-08-03 01:42:28 +0300

    homework_30_2 ex-1

--- course/serializers.py
+++ course/serializers.py
@@ -4,10 +4,14 @@ from course.models import Course, Lesson
 
 
 class CourseSerializers(serializers.ModelSerializer):
+    quant_les = serializers.SerializerMethodField()
 
     class Meta:
         model = Course
         fields = '__all__'
+
+    def get_quant_les(self, obj):
+        return obj.lesson_set.count()
 
 class LessonSerializers(serializers.ModelSerializer):
 
